5e61cad8d1d68f8b0da0faec1fb39e70e98d0023
PavanKumarBollu <bollupavankumar12@gmail.com>
2026-03-11 17:57:45 +0530

    SqLDDL

--- SQL/Basics.sql
+++ SQL/Basics.sql
@@ -2,4 +2,20 @@
 
 CREATE DATABASE IF NOT EXISTS dpba2957;
 
+USE dpba2957;
 
+-- CREATE TABLE  users(
+-- sno int primary key,
+-- stdfname varchar(50), 
+-- stdlname varchar(50),
+-- qualification varchar(50),
+-- gender varchar(5)
+-- );
+
+CREATE TABLE IF NOT EXISTS users(
+sno int primary key,
+stdfname varchar(50), 
+stdlname varchar(50),
+qualification varchar(50),
+gender varchar(5)
+);
